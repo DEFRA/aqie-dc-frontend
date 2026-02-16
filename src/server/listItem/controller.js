c00f5528e2e92f7d4bbf876bc74b9b859b69e68f
CorasCode <corascompanycode@gmail.com>
2026-02-16 16:14:05 +0000

    Add UI outline

--- src/server/listItem/controller.js
+++ src/server/listItem/controller.js
@@ -5,6 +5,13 @@ import { content } from './content.js'
  */
 export const listItemController = {
   handler(_request, h) {
-    return h.view('listItem/index', content)
+    return (
+      h.view('listItem/index', content),
+      {
+        pageTitle: `?`,
+        heading: `Name of the item`,
+        manufacturer: `Manufactured by:` //${}`
+      }
+    )
   }
 }
